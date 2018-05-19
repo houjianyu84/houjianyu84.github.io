97dd54582832268a024c5d1dd3d8850c6c4e45c3
houjianyu84 <houjianyu84@gmail.com>
2018-05-19 02:20:55 -0400

    Site updated: 2018-05-19 02:20:54

--- js/search.js
+++ js/search.js
@@ -1,147 +1,90 @@
-// A local search script with the help of [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
-// Copyright (C) 2015 
-// Joseph Pan <http://github.com/wzpan>
-// Shuhao Mao <http://github.com/maoshuhao>
-// This library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2.1 of the
-// License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA
-// 
-var searchAll;
-var searchFunc = function (path, search_id, content_id) {
-  'use strict';
-  $.ajax({
-    url: path,
-    dataType: 'xml',
-    complete: function (xmlResponse) {
-      // get the contents from search data
-        /* 由于 xml 不支持特殊字符，所有手动转 */
-        function createXml(str){
-            if(document.all){
-                var xmlDom=new ActiveXObject("Microsoft.XMLDOM");
-                xmlDom.loadXML(str);
-                return xmlDom;
-            }
-            else
-                return new DOMParser().parseFromString(str, "text/xml");
-        }
-      var datas = $("entry", createXml(xmlResponse.responseText)).map(function () {
-        return {
-          title: $("title", this).text(),
-          content: $("content", this).text(),
-          url: $("url", this).text()
-        };
-      }).get();
+var lunrIndex, pagesIndex;
 
-      var $input = document.getElementById(search_id);
-      var $resultContent = document.getElementById(content_id);
-
-      searchAll = function (val) {
-        var str = '<ul class=\"search-result-list\">';
-        var keywords = val.trim().toLowerCase().trim().split(/[\s\-]+/);
-        $resultContent.innerHTML = "";
-        if (val.trim().length <= 0) {
-          return;
-        }
-        // perform local searching
-        datas.forEach(function (data) {
-          var isMatch = true;
-          var content_index = [];
-          if (!data.title || data.title.trim() === '') {
-            data.title = "Untitled";
-          }
-          var data_title = data.title.trim().toLowerCase();
-          var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
-          var data_url = data.url;
-          var index_title = -1;
-          var index_content = -1;
-          var first_occur = -1;
-          // only match artiles with not empty contents
-          if (data_content !== '') {
-            keywords.forEach(function (keyword, i) {
-              index_title = data_title.indexOf(keyword);
-              index_content = data_content.indexOf(keyword);
-
-              if (index_title < 0 && index_content < 0) {
-                isMatch = false;
-              } else {
-                if (index_content < 0) {
-                  index_content = 0;
-                }
-                if (i == 0) {
-                  first_occur = index_content;
-                }
-                // content_index.push({index_content:index_content, keyword_len:keyword_len});
-              }
-            });
-          } else {
-            isMatch = false;
-          }
-          // show search results
-          if (isMatch) {
-            var urls = data_url.split("/");
-            var post_date = urls[1]+"/"+urls[2]+"/"+urls[3];
-            str += "<li><a href='" + data_url + "'><span class='post-title' title='"+data_title+"'>" + data_title + "</span><span class='post-date' title='"+post_date+"'>"+post_date+"</span></a>";
-            var content = data.content.trim().replace(/<[^>]+>/g, "");
-            if (first_occur >= 0) {
-              // cut out 100 characters
-              var start = first_occur - 20;
-              var end = first_occur + 80;
-
-              if (start < 0) {
-                start = 0;
-              }
-
-              if (start == 0) {
-                end = 100;
-              }
-
-              if (end > content.length) {
-                end = content.length;
-              }
-
-              var match_content = content.substr(start, end);
-
-              // highlight all keywords
-              keywords.forEach(function (keyword) {
-                var regS = new RegExp(keyword, "gi");
-                match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
-              });
-
-              str += "<p class=\"search-result\">" + match_content + "...</p>"
-            }
-            str += "</li>";
-          }
-        });
-        str += "</ul>";
-        if (str.indexOf('<li>') === -1) {
-          return $resultContent.innerHTML = "<ul><span class='local-search-empty'>没有找到内容，更换下搜索词试试吧~<span></ul>";
-        }
-        $resultContent.innerHTML = str;
-
-          $(document).pjax('#local-search-result a', '.pjax', {fragment: '.pjax', timeout: 8000});
-          /*鼠标移出文章列表后，去掉文章标题hover样式*/
-          $("#local-search-result a").mouseenter(function (e) {
-              $("#local-search-result a.hover").removeClass("hover");
-              $(this).addClass("hover");
-          });
-          $("#local-search-result a").mouseleave(function (e) {
-              $(this).removeClass("hover");
-          });
-      }
-    },
-      error: function (XMLHttpRequest, textStatus, errorThrown) {
-          console.log('文章中出现特殊字符，导致解析xml出现问题，系统自动采用第二方案：进行主动解析！！！ 请检查全文搜索是否有问题，如出现问题，请及时在 https://github.com/yelog/hexo-theme-3-hexo/issues 中提出来，作者会尽快处理！')
-      }
-  });
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
+
+// Initialize lunrjs using our generated index file
+function initLunr() {
+    if (!endsWith(baseurl,"/")){
+        baseurl = baseurl+'/'
+    };
+
+    // First retrieve the index file
+    $.getJSON(baseurl +"index.json")
+        .done(function(index) {
+            pagesIndex =   index;
+            // Set up lunrjs by declaring the fields we use
+            // Also provide their boost level for the ranking
+            lunrIndex = new lunr.Index
+            lunrIndex.ref("uri");
+            lunrIndex.field('title', {
+                boost: 15
+            });
+            lunrIndex.field('tags', {
+                boost: 10
+            });
+            lunrIndex.field("content", {
+                boost: 5
+            });
+
+            // Feed lunr with each file and let lunr actually index them
+            pagesIndex.forEach(function(page) {
+                lunrIndex.add(page);
+            });
+            lunrIndex.pipeline.remove(lunrIndex.stemmer)
+        })
+        .fail(function(jqxhr, textStatus, error) {
+            var err = textStatus + ", " + error;
+            console.error("Error getting Hugo index flie:", err);
+        });
+}
+
+/**
+ * Trigger a search in lunr and transform the result
+ *
+ * @param  {String} query
+ * @return {Array}  results
+ */
+function search(query) {
+    // Find the item in our index corresponding to the lunr one to have more info
+    return lunrIndex.search(query).map(function(result) {
+            return pagesIndex.filter(function(page) {
+                return page.uri === result.ref;
+            })[0];
+        });
+}
+
+// Let's get started
+initLunr();
+$( document ).ready(function() {
+    var searchList = new autoComplete({
+        /* selector for the search box element */
+        selector: $("#search-by").get(0),
+        /* source is the callback to perform the search */
+        source: function(term, response) {
+            response(search(term));
+        },
+        /* renderItem displays individual search results */
+        renderItem: function(item, term) {
+            var numContextWords = 2;
+            var text = item.content.match(
+                "(?:\\s?(?:[\\w]+)\\s?){0,"+numContextWords+"}" +
+                    term+"(?:\\s?(?:[\\w]+)\\s?){0,"+numContextWords+"}");
+            item.context = text;
+            return '<div class="autocomplete-suggestion" ' +
+                'data-term="' + term + '" ' +
+                'data-title="' + item.title + '" ' +
+                'data-uri="'+ item.uri + '" ' +
+                'data-context="' + item.context + '">' +
+                '» ' + item.title +
+                '<div class="context">' +
+                (item.context || '') +'</div>' +
+                '</div>';
+        },
+        /* onSelect callback fires when a search suggestion is chosen */
+        onSelect: function(e, term, item) {
+            location.href = item.getAttribute('data-uri');
+        }
+    });
+});
